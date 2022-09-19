d5636314b85d5e38ef5c0f18efdc28130c9a9963
OGvital <ogbanudavid19@gmail.com>
2022-09-19 15:16:41 +0000

    hg

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _atoi(char *s);
+char *_strcpy(char *dest, char *scr);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+void _puts(char *str);
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_X10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+#endif /* MAIN_H */
+
