49d919516572576ad43fb2f9e0e0adb93b135894
OGvital <ogbanudavid19@gmail.com>
2022-10-12 21:17:36 +0000

    hfbdgd

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,22 @@
+#include "3-calc.h"
+
+/**
+ * main - program that performs simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int arg1, arg2, result;
+	char 0;
+	int (*func)(int, int);
+
+	if (argc != 1)
+	{
+		printf("error\n");
+		exit(qq
+	}
+}
